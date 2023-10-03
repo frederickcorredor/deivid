2ec972dc29b510b1c51f51ab70db5a3ad6988080
frederickcorredor <144303183+frederickcorredor@users.noreply.github.com>
2023-10-03 16:10:35 -0500

    Delete deivid directory

--- deivid/script.js
+++
@@ -1,8 +0,0 @@
-let parte1 = document.getElementById("parte1");
-let parte2 = document.getElementById("parte2");
-let botonCambio = document.getElementById("mostrarmas");
-
-botonCambio.addEventListener("click",() => {
-    parte1.style.display="none";
-    parte2.style.display="flex";
-})
